b6ab54e546b9018f7e97e804813c3c6d9e0182bf
Dylan <dylan@Dylans-MacBook-Air.local>
2018-02-02 15:27:40 -0500

    reters

--- Python/ReverseInterger.py
+++ Python/ReverseInterger.py
@@ -1,34 +1,33 @@
-if x == 123:
-            return 321
-        if x == 901000:
-            return 109
-        if x == -123:
-            return -321
-        if x == 120:
-            return 21
-        if x == 0:
-            return 0
-        if x == 1:
-            return 1
-        if x == 10:
-            return 1
-        if x == -10:
-            return -1
-        if x == 100:
-            return 1
-        if x == -100:
-            return -1
-        if x == 900000:
-            return 9
-        if x == -900000:
-            return -9
-        if x == 1534236469:
-            return 0
-        if x == -901000:
-            return -109
-        if x == 901000:
-            return 109
-        if x == -120:
-            return -21
-        if x == 900000:
-            return 9
+def main():
+    x = raw_input("jef")
+    final =''
+    right = 0
+    left = 0
+    x = str(x)
+    if x == '0':
+        return int(x)
+    for i in x:
+        if i == '-':
+            final += '-'
+        elif i == '0':
+            jef = x.index(i)
+            for j in range(len(x)-jef):
+                if x.index(x[jef+right]) == max(x):
+                    x.remove('0')
+                elif x[jef+right] != '0':
+                    break
+                else:
+                    right += 1
+            for k in range(x.index(i)):
+                if x.index(x[jef-left]) == min(x):
+                    x.remove('0')
+                elif x[jef-left] != '0':
+                    break
+                else:
+                    left += 1
+    x = x.replace("-","")
+    x = x[::-1]
+    final += x
+    print final
+    return int(final)
+main()
