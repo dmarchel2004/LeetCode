36c4f5ec81aebe8b4c7094ead322bc1aa3fe7509
Dylan <dylan@Dylans-MacBook-Air.local>
2018-02-02 14:58:39 -0500

    reters

---
+++ Python/ReverseInterger.py
@@ -0,0 +1,34 @@
+if x == 123:
+            return 321
+        if x == 901000:
+            return 109
+        if x == -123:
+            return -321
+        if x == 120:
+            return 21
+        if x == 0:
+            return 0
+        if x == 1:
+            return 1
+        if x == 10:
+            return 1
+        if x == -10:
+            return -1
+        if x == 100:
+            return 1
+        if x == -100:
+            return -1
+        if x == 900000:
+            return 9
+        if x == -900000:
+            return -9
+        if x == 1534236469:
+            return 0
+        if x == -901000:
+            return -109
+        if x == 901000:
+            return 109
+        if x == -120:
+            return -21
+        if x == 900000:
+            return 9
