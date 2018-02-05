77839c60bfbbb7704e887edbed4efd29ab571c97
Dylan <dylan@Dylans-MacBook-Air.local>
2018-02-05 13:21:19 -0500

    oov

--- Python/ReverseInterger.py
+++ Python/ReverseInterger.py
@@ -1,17 +1,19 @@
 def zero(l, j):
+    right = 0
+    left = 0
     jef = l.index(j)
     for j in range(len(l)-jef):
         if l.index(l[jef+right]) == max(l):
-            l.pop(j)
+            return False
         elif l[jef+right] != '0':
-            break
+            return True
         else:
             right += 1
     for k in range(l.index(j)):
         if l.index(l[jef-left]) == min(l):
-            l.pop(j)
+            return False
         elif l[jef-left] != '0':
-            break
+            return True
         else:
             left += 1
 
@@ -19,16 +21,15 @@ def zero(l, j):
 def main():
     x = raw_input("jef")
     final =''
-    right = 0
-    left = 0
     x = str(x)
-    if x == '0':
+    if x == '0' or int(x) > 2147483647:
         return int(x)
     for i in x:
         if i == '-':
             final += '-'
         elif i == '0':
-            zero(x, i)
+            if zero(x, i) == False:
+                x.pop(i)
     x = x.replace("-","")
     x = x[::-1]
     final += x
