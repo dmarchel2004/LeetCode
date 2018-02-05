fd940a67c6380a29be3cbb04b87a59bb3f3e3c56
Dylan <dylan@Dylans-MacBook-Air.local>
2018-02-05 18:10:57 -0500

    yure yure daze

--- Python/ReverseInterger.py
+++ Python/ReverseInterger.py
@@ -2,52 +2,51 @@ def zero(l, j):
     right = 0
     left = 0
     jef = l.index(j)
-    good = 0
-    print "hi"
-    print len(l)
-    for m in range(len(l)-jef):
-        print"hj"
-        if l[jef+right] == max(l):
-            print "hb"
-            return False
-        elif l[jef+right] != '0':
-            print "hg"
-            good = 1
-        else:
-            right += 1
-            print "hc"
-    for k in range(l.index(j)):
-        if l[jef-left] == min(l):
-            return False
-        elif l[jef-left] != '0':
-            good = 2
-        else:
-            left += 1
-        if good == 2:
+    print 'jef', jef
+    while True:
+        print right
+        print left
+        # print l[jef+right]
+        # print l[jef-left]
+        if l[jef+right] != '0' and l[jef-left] != '0':
+            print 'o'
             return True
+        elif l[jef+right] == '0' and jef + right + 1!= len(l):
+            right +=1
+            print 'f'
+        elif l[jef - left] == '0' and jef - left != 0:
+            left += 1
+            print 'of'
+        elif (l[jef+right] == '0' and jef + right + 1 == len(l)) or (l[jef - left] == '0' and jef - left == 0):
+            print 'oof'
+            return False
 
-
-
-def main():
-    x = raw_input("jef")
+def reverse():
+    x = raw_input("ppf")
     final =''
-    x = str(x)
     zerolist = []
+    removals = []
     if x == '0' or int(x) > 2147483647:
-        return int(x)
+        return 0
     for s in x:
         zerolist.append(s)
-        for i in zerolist:
-            if i == '-':
-                final += '-'
-                zerolist.remove(i)
-            elif i == '0':
-                if zero(zerolist, i) == False:
-                    zerolist.remove(i)
-            print zerolist
+    for i in zerolist:
+        print 'i', i
+        print i, zerolist.index(i)
+        if i == '-':
+            final += '-'
+            zerolist.remove(i)
+        elif i == '0':
+            if zero(zerolist, i) == False:
+                removals.append(i)
+            else:
+                removals.append('')
+    place = 0
+    for i in zerolist:
+        if i == removals[0]:
+            zerolist.remove(i)
     zerolist = zerolist[::-1]
     for c in zerolist:
-        print final
         final += c
     print final
-main()
+reverse()
