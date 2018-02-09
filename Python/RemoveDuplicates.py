5d36dfa635b9d0b934a14bda3ce52ca698f389e8
Dylan <dylan@Dylans-MacBook-Air.local>
2018-02-09 13:59:59 -0500

    finished another

---
+++ Python/RemoveDuplicates.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        numTot = nums1 + nums2
+        numTot.sort()
+        if len(numTot) % 2.0 == 0:
+            return (numTot[len(numTot)/2]+numTot[(len(numTot)/2)-1])/2.0
+        else:
+            return numTot[len(numTot)/2]
